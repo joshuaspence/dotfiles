c721a0b057f438cdb45d3b5d36d41d74e97bdea5
Joshua Spence <josh@joshuaspence.com>
2012-07-20 10:23:21 +1000

    Further work on parse.pl

--- packages/parse.pl
+++ packages/parse.pl
@@ -38,26 +38,33 @@ open FILE, "<", $package_file or die $!;
 
 # Parse the data
 my $line;
-my $new_package = false;
+my $header = '';
 while($line = <FILE>) {
     chomp($line); # remove newline characters
     
-    if ($line = "") {
-        $new_package = true;
-    } else if ($line =~ m/^\s*#/) {
+    if ($line =~ m/^\s*\$/) {
         # This is a header
-        # or a comment
-        (my $header = $line) =~ s/\s*#\s*//;
-        
-        print "Header: '$header'\n";
-    } else {
+        ($header = $line) =~ s/\s*\$\s*//;
+        $all_packages{$header} = ();
+        $all_packages{$header}{'comment'} = '';
+        $all_packages{$header}{'packages'} = [];
+        print('1\n');
+    } elsif ($line =~ m/^\s*#/ && $header !~ m/^$/) {
+        # This is a comment
+        (my $comment = $line) =~ s/^[^#]*(#\s*)?//;
+        $all_packages{$header}{'comment'} = $comment;
+        print('2\n');
+    } elsif ($header !~ m/^$/) {
         # This is data
         (my $package = $line) =~ s/\s*#.*$//;
         (my $comment = $line) =~ s/^[^#]*(#\s*)?//;
-        
-        print "Package: '$package'\n";
-        print "Comment: '$comment'\n";
+        push($all_packages{$header}{'packages'},$package);
+        print('3\n');
     }
+}
+
+foreach my $package (%all_packages) {
+    print("$package\n");
 }
 
 # Perl trim function to remove whitespace from the start and end of the string
