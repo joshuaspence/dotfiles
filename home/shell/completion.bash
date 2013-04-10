1cd92036fb3be23cbf090a81b9d17506a0c68e43
Joshua Spence <josh@joshuaspence.com>
2013-04-10 18:35:45 +1000

    Changed tabs to changes

--- home/shell/completion.bash
+++ home/shell/completion.bash
@@ -1,8 +1,8 @@
 # Enable programmable completion features.
 if ! shopt -oq posix; then
-	if [[ -r /usr/share/bash-completion/bash_completion ]]; then
-		source /usr/share/bash-completion/bash_completion
+    if [[ -r /usr/share/bash-completion/bash_completion ]]; then
+        source /usr/share/bash-completion/bash_completion
     elif [[ -r /etc/bash_completion ]]; then
-    	source /etc/bash_completion
+        source /etc/bash_completion
     fi
 fi
