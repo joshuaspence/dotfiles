025524e3ce114c7bb5a22e6d65c7956067c3241e
Joshua Spence <josh@joshuaspence.com>
2013-04-12 08:15:18 +1000

    Changes to hell environment. Changed GIT_EDITOR to vim

--- home/shell/environment/ls.zsh
+++ home/shell/environment/ls.zsh
@@ -1,8 +1,14 @@
+##
 # Unset environment variables.
+##
 unset DISABLE_LS_COLORS
 
+##
 # Make sure `ls` is installed.
+##
 command -v ls >/dev/null || return
 
-# Set and export environment variables.
+##
+# Set environment variables.
+##
 DISABLE_LS_COLORS='false'
