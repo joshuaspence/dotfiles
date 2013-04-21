b9c9c8aa5d73eb736735d8087f43f08a4242c1a7
Joshua Spence <josh@joshuaspence.com>
2013-04-22 00:38:17 +1000

    Added some stuff from the interwebs

---
+++ home/shell/environment/os.sh
@@ -0,0 +1,30 @@
+#/
+## @author Joshua Spence
+## @file   ~/.shell/environment/os.sh
+## 
+## @link   http://github.com/mikemcquaid/dotfiles
+#\
+
+## Unset environment variables. #{{{
+    unset LINUX
+    unset OSX
+    unset UNIX
+    unset WINDOWS
+## #}}}
+
+if [[ $(uname -s) == 'Darwin' ]]; then
+    OSX=1
+    UNIX=1
+elif [[ $(uname -s) == 'Linux' ]]; then
+    LINUX=1
+    UNIX=1
+elif [[ $(uname -s) == '*_NT-*' ]]; then
+    WINDOWS=1
+fi
+
+## Export environment variables. #{{{
+    export LINUX
+    export OSX
+    export UNIX
+    export WINDOWS
+## #}}}
