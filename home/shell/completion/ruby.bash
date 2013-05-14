380669e23233381f5770873b8b4734853259a760
Joshua Spence <josh@joshuaspence.com>
2013-05-14 10:06:36 +1000

    Added bash completion for Ruby

---
+++ home/shell/completion/ruby.bash
@@ -0,0 +1,11 @@
+#/
+## {@link http://www.gnu.org/software/bash/ bash} completion for
+## {@link http://www.ruby-lang.org/ Ruby}.
+##
+## @author Joshua Spence
+## @file   ~/.shell/completion/rvm.bash
+#\
+
+if [[ -f $HOME/.shell/completion/ruby/completion-ruby-all ]]; then
+    source "${HOME}/.shell/completion/ruby/completion-ruby-all"
+fi
