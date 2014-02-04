317eb575550a27d0f37b4d911426e6d5a1973d7e
Joshua Spence <josh@joshuaspence.com>
2014-02-05 08:21:40 +1100

    Removed `rot13` shell alias

--- home/shell/aliases/string/rot13.sh
+++
@@ -1,8 +0,0 @@
-#/
-## A shell alias which acts as a ROT13 encoder/decoder.
-#\
-
-## ROT13 encoder/decoder. The input should be piped into this alias.
-##
-## @link https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
-alias rot13='tr a-zA-Z n-za-mN-ZA-M'
