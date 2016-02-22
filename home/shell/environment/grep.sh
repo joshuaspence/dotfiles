c0436e9656a7bdf3cb98c201e05917ffe1d17b49
Joshua Spence <josh@freelancer.com>
2016-02-22 19:53:54 +1100

    Remove deprecated `GREP_OPTIONS`

--- home/shell/environment/grep.sh
+++ home/shell/environment/grep.sh
@@ -7,9 +7,7 @@ command -v grep &>/dev/null && {
 
     if $CLICOLOR; then
         export GREP_COLORS='ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36'
-        export GREP_OPTIONS='--color=auto'
     else
         unset GREP_COLORS
-        unset GREP_OPTIONS
     fi
 }
