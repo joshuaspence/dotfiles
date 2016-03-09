33783a95ac7cc5c4742ae22878787b4e73233fa2
Joshua Spence <josh@freelancer.com>
2016-03-09 21:57:25 +1100

    Set architecture for Google Chrome apt source
    See http://www.omgubuntu.co.uk/2016/03/fix-failed-to-fetch-google-chrome-apt-error-ubuntu.

--- puppet/modules/google_chrome/manifests/init.pp
+++ puppet/modules/google_chrome/manifests/init.pp
@@ -7,22 +7,23 @@ class google_chrome(
   validate_re($ensure, '^(absent|present|latest)$')
 
   apt::source { 'google-chrome':
-    ensure   => $ensure ? {
+    ensure       => $ensure ? {
       absent  => 'absent',
       present => 'present',
       latest  => 'present',
     },
-    location => 'http://dl.google.com/linux/chrome/deb/',
-    release  => 'stable',
-    repos    => 'main',
-    include  => {
+    location     => 'http://dl.google.com/linux/chrome/deb/',
+    release      => 'stable',
+    repos        => 'main',
+    include      => {
       deb => true,
       src => false,
     },
-    key      => {
+    key          => {
       id     => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
       source => 'https://dl.google.com/linux/linux_signing_key.pub',
     },
+    architecture => 'amd64',
   }
 
   package { 'google-chrome-stable':
