b8fd272ad5d6e9fb3505440d06d182bfe07b4dbc
Joshua Spence <josh@freelancer.com>
2018-07-26 18:10:59 +1000

    Configure shell history

---
+++ home/shell/config/history.bash
@@ -0,0 +1,16 @@
+# Append to the history , don't overwrite it.
+shopt -s histappend
+
+# Entries beginning with space aren't added into history, and duplicate
+# entries will be erased (leaving the most recent entry).
+HISTCONTROL='ignorespace:erasedups'
+
+# Lots of history. 
+HISTSIZE=10000000000
+HISTFILESIZE="${HISTSIZE}"
+
+# File where the history is stored.
+HISTFILE="${HOME}/.bash_history"
+
+# Exclude certain commands from the history file.
+HISTIGNORE='cd:history:ls:pwd'
