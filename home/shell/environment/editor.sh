10974c836c62f13c41f1d0d3af853074afd3d2dc
Joshua Spence <josh@freelancer.com>
2018-07-27 12:36:21 +1000

    Set `$EDITOR`

---
+++ home/shell/environment/editor.sh
@@ -0,0 +1,8 @@
+if [[ -n $DISPLAY ]] && command -v subl &>/dev/null; then
+  export EDITOR='subl --new-window --wait'
+elif command -v vim &>/dev/null; then
+  export EDITOR='vim'
+else
+  unset EDITOR
+  echo 'No command set for $EDITOR' >&2
+fi
