dea9afd1f53b088fcf4bbe411e8e38c2c4e6a5e2
Joshua Spence <josh@freelancer.com>
2018-07-29 12:04:49 +1000

    A few minor fixes for PS1

--- home/shell/config/prompt.bash
+++ home/shell/config/prompt.bash
@@ -3,12 +3,12 @@ function shell_prompt() {
   local -r joiner_bottomleft=$'\xE2\x94\x94'
   local -r joiner_topleft=$'\xE2\x94\x8C'
   local -r separator=$'\xE2\x94\x80'
-  local -r separator_tail=$'\xE2\x94\xBC'
+  local -r separator_tail=$'\xE2\x95\xBC'
   local -r tick=$'\xE2\x9C\x93'
 
   local -r host="\[${COLOR_CYAN}\]\H\[${COLOR_NONE}\]"
   local -r pwd="\[${COLOR_GREEN}\]\w\[${COLOR_NONE}\]"
-  local -r status="\[\$([[ \$? != 0 ]] && echo -n '${BOLDCOLOR_RED}${cross}${COLOR_NONE}' || echo -n '${BOLDCOLOR_GREEN}${tick}${COLOR_NONE}')\]"
+  local -r status="\[\$([[ \$? == 0 ]] && echo -n '${BOLDCOLOR_GREEN}${tick}${COLOR_NONE}' || echo -n '${BOLDCOLOR_RED}${cross}${COLOR_NONE}')\]"
   local -r time="\[${COLOR_BLUE}\]\t\[${COLOR_NONE}\]"
   local -r user="\[${COLOR_YELLOW}\]\u\[${COLOR_NONE}\]"
 
