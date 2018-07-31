912dde91c1c2131685cfea8b9b8107d95efb7c19
Joshua Spence <josh@freelancer.com>
2018-08-01 08:36:29 +1000

    Tweak `$PAGER`

--- home/shell/environment/pager.sh
+++ home/shell/environment/pager.sh
@@ -1,7 +1,7 @@
 if command -v less &>/dev/null; then
-  export PAGER=$(command -v less)
+  export PAGER='less --ignore-case --chop-long-lines'
 elif command -v more &>/dev/null; then
-  export PAGER=$(command -v more)
+  export PAGER='more'
 else
   unset PAGER
   echo 'No command set for $PAGER' >&2
