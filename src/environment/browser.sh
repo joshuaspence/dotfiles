eba1194476cb04e60d867d0dc014609ca84d2dee
Joshua Spence <josh@joshuaspence.com>
2018-09-06 08:39:59 +1000

    Minor

--- src/environment/browser.sh
+++ src/environment/browser.sh
@@ -1,9 +1,9 @@
 if test -n "${DISPLAY}" && command -v google-chrome &>/dev/null; then
-  export BROWSER=google-chrome
+  export BROWSER='google-chrome'
 elif test -n "${DISPLAY}" && command -v firefox &>/dev/null; then
-  export BROWSER=firefox
+  export BROWSER='firefox'
 elif command -v lynx &>/dev/null; then
-  export BROWSER=lynx
+  export BROWSER='lynx'
 else
   unset BROWSER
   echo 'No command set for $BROWSER' >&2
