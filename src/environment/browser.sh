b353c87de134b2dfc9b2371973395179993ec8ce
Joshua Spence <josh@joshuaspence.com>
2018-09-06 08:32:34 +1000

    Don't use `[[ ... ]]` since it's Bash-specific

--- src/environment/browser.sh
+++ src/environment/browser.sh
@@ -1,6 +1,6 @@
-if [[ -n $DISPLAY ]] && command -v google-chrome &>/dev/null; then
+if test -n "${DISPLAY}" && command -v google-chrome &>/dev/null; then
   export BROWSER=google-chrome
-elif [[ -n $DISPLAY ]] && command -v firefox &>/dev/null; then
+elif test -n "${DISPLAY}" && command -v firefox &>/dev/null; then
   export BROWSER=firefox
 elif command -v lynx &>/dev/null; then
   export BROWSER=lynx
--- src/environment/ec2-scripts.sh
+++ src/environment/ec2-scripts.sh
@@ -1,3 +1,3 @@
-if [[ -d "${HOME}/ec2-scripts" ]]; then
+if test -d "${HOME}/ec2-scripts"; then
   export PATH="${PATH}:${HOME}/ec2-scripts/bin"
 fi
