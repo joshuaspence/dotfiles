c96f40bd757d796b64dd4dc5ef19e2c243b2172e
Joshua Spence <josh@joshuaspence.com>
2018-09-07 14:25:31 +1000

    Tweak `$FIGNORE`

--- src/config/fignore.bash
+++ src/config/fignore.bash
@@ -1,4 +1,4 @@
 # `$FIGNORE` is a colon-separated list of suffixes to ignore when performing
 # filename completion. A filename whose suffix matches one of the entries in
 # `$FIGNORE` is excluded from the list of matched filenames.
-unset FIGNORE
+FIGNORE='~:.o:.pyc:.swp'
