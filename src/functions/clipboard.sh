ab1e41f5a0abf3c8871ad8f01bf662ebedbfab45
Joshua Spence <josh@joshuaspence.com>
2018-09-07 11:32:49 +1000

    Minor

--- src/functions/clipboard.sh
+++ src/functions/clipboard.sh
@@ -1,5 +1,5 @@
 # TODO: It would be nice if we could add `-in` or `-out` automatically based
 # on input/output redirection.
-command -v xclip &>/dev/null && {
+if command -v xclip &>/dev/null; then
   alias clipboard='xclip -selection clipboard'
-}
+fi
