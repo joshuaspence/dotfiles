fee4d02c540d7f6fd637cea3fc499bd502a36cdb
Joshua Spence <josh@joshuaspence.com>
2018-09-07 11:41:18 +1000

    Minor tidying of `virtulenv` initialization

--- src/environment/virtualenv.sh
+++ src/environment/virtualenv.sh
@@ -1,14 +1,8 @@
-if ! test -d "${HOME}/.venv"; then
-  virtualenv "${HOME}/.venv"
-fi
-
-# Don't include virtualenv name in prompt.
-# See https://github.com/pypa/virtualenv/issues/5.
-# shellcheck disable=SC2034
-VIRTUAL_ENV_DISABLE_PROMPT=1
-
 if test -f "${HOME}/.venv/bin/activate"; then
+  # Don't include virtualenv name in prompt
+  # (see https://github.com/pypa/virtualenv/issues/5).
+  #
+  # shellcheck disable=SC2034
+  VIRTUAL_ENV_DISABLE_PROMPT=1
   source "${HOME}/.venv/bin/activate"
 fi
-
-# TODO: Automatically/periodically install dependencies.
