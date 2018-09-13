76a42d8bc4adf6a64693a89a892d93f7e30e2364
Joshua Spence <josh@joshuaspence.com>
2018-09-13 10:14:28 +1000

    Fix `$PATH`
    For some reason, `virtualenv` is resetting `$PATH`.

--- src/profile.sh
+++ src/profile.sh
@@ -5,4 +5,7 @@ if [ -n "${BASH_VERSION}" ]; then
   fi
 fi
 
+# For some reason, `virtualenv` is resetting `$PATH`. As such, we must setup
+# `virtualenv` before appending/prepending to `$PATH`.
+include environment/virtualenv.sh
 includex environment/**.sh
