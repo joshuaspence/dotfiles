f1795be74c1c046408cef9dc8735bf369cf3f96c
Joshua Spence <josh@joshuaspence.com>
2018-09-19 12:12:04 +1000

    Remove `src/config/colors.sh`

--- src/config/prompt.bash
+++ src/config/prompt.bash
@@ -1,18 +1,26 @@
-include config/colors.sh
-
 PS1=$(
-  readonly cross=$'\xE2\x9C\x97'
+  readonly color_none='\e[0m'
+  readonly color_red='\e[0;31m'
+  readonly color_green='\e[0;32m'
+  readonly color_yellow='\e[0;33m'
+  readonly color_blue='\e[0;34m'
+  readonly color_cyan='\e[0;36m'
+  readonly color_white='\e[0;37m'
+  readonly boldcolor_red='\e[1;31m'
+  readonly boldcolor_green='\e[1;32m'
+
+  readonly cross='\xE2\x9C\x97'
   readonly joiner_bottomleft=$'\xE2\x94\x94'
   readonly joiner_topleft=$'\xE2\x94\x8C'
   readonly separator=$'\xE2\x94\x80'
   readonly separator_tail=$'\xE2\x95\xBC'
-  readonly tick=$'\xE2\x9C\x93'
+  readonly tick='\xE2\x9C\x93'
 
-  readonly host="\[${COLOR_CYAN}\]\[\$([[ -n \$SSH_CONNECTION ]] && printf '\H' || printf 'localhost')\]\[${COLOR_NONE}\]"
-  readonly pwd="\[${COLOR_GREEN}\]\w\[${COLOR_NONE}\]"
-  readonly status="\[\$([[ \$? == 0 ]] && printf '${BOLDCOLOR_GREEN}${tick}${COLOR_NONE}' || printf '${BOLDCOLOR_RED}${cross}${COLOR_NONE}')\]"
-  readonly time="\[${COLOR_BLUE}\]\t\[${COLOR_NONE}\]"
-  readonly user="\[\$([[ \$EUID == 0 ]] && printf '${COLOR_RED}' || printf '${COLOR_YELLOW}')\]\u\[${COLOR_NONE}\]"
+  readonly host="\[${color_cyan}\]\[\$([[ -n \$SSH_CONNECTION ]] && printf '\H' || printf 'localhost')\]\[${color_none}\]"
+  readonly pwd="\[${color_green}\]\w\[${color_none}\]"
+  readonly status="\[\$([[ \$? == 0 ]] && printf '${boldcolor_green}${tick}${color_none}' || printf '${boldcolor_red}${cross}${color_none}')\]"
+  readonly time="\[${color_blue}\]\t\[${color_none}\]"
+  readonly user="\[\$([[ \$EUID == 0 ]] && printf '${color_red}' || printf '${color_yellow}')\]\u\[${color_none}\]"
 
   echo -n "${joiner_topleft}${separator}"
   echo -n "[${status}]"
