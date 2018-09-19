a942a26e41babc2560e8a000882621004677d6b7
Joshua Spence <josh@joshuaspence.com>
2018-09-19 12:28:30 +1000

    Tweak bash history

--- src/config/history.bash
+++ src/config/history.bash
@@ -3,15 +3,12 @@ HISTFILESIZE=1048576
 HISTSIZE=1048576
 
 # Exclude certain commands from the history file.
-HISTIGNORE='[bf]g:clear:exit:history:ls:pwd'
+HISTIGNORE='[bf]g:cd *:clear:exit:history:ls:pwd'
 
 # Whenever displaying the prompt, write the previous line to disk.
 # See https://unix.stackexchange.com/a/48113.
 PROMPT_COMMAND='history -w'
 
-# Save all lines of a multiple-line command in the same history entry.
 shopt -s cmdhist
-shopt -s lithist
-
-# Append to the history file, don't overwrite it.
 shopt -s histappend
+shopt -s lithist
