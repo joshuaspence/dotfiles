10d8917deeb94174e8e441b226a3574163227415
Joshua Spence <jspence2@c02t70q3gygr.office.atlassian.com>
2019-08-05 15:27:27 +1000

    Add aliases for `python` and `pip` when Python has been installed using Homebrew

--- src/environment/brew.sh
+++ src/environment/brew.sh
@@ -1,3 +1,7 @@
 if test -d /usr/local/opt/findutils/libexec/gnubin; then
   export PATH="/usr/local/opt/findutils/libexec/gnubin:${PATH}"
 fi
+
+if test -d /usr/local/opt/python/libexec/bin; then
+  export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
+fi
