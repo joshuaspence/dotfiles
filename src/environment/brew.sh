b2701b3bdc45662bd4c23fde5c0b450bcf71130d
Joshua Spence <josh@joshuaspence.com>
2019-08-27 11:55:00 +1000

    Use `gnu-getopt`
    When using Mac OS, use GNU `getopt`.

--- src/environment/brew.sh
+++ src/environment/brew.sh
@@ -2,6 +2,10 @@ if test -d /usr/local/opt/findutils/libexec/gnubin; then
   export PATH="/usr/local/opt/findutils/libexec/gnubin:${PATH}"
 fi
 
-if test -d /usr/local/opt/python/libexec/bin; then
+if test -d /usr/local/opt/gnu-getopt; then
+  export PATH="/usr/local/opt/gnu-getopt/bin:${PATH}"
+fi
+
+if test -d /usr/local/opt/python/libexec; then
   export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
 fi
