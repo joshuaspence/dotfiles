98c4ebf4aac41e785a9767eb04e7012877d9da48
Joshua Spence <josh@joshuaspence.com>
2019-08-27 13:52:43 +1000

    Guard some `shopt` options that require Bash 4

--- src/config/opts.bash
+++ src/config/opts.bash
@@ -1,8 +1,12 @@
 shopt -s cdspell
 shopt -s checkhash
 shopt -s checkwinsize
-shopt -s dirspell
 shopt -s extglob
-shopt -s globstar
 shopt -s histreedit
 shopt -s histverify
+
+# These options are only supported on Bash 4.
+if [[ ${BASH_VERSINFO[0]} -ge 4 ]]; then
+  shopt -s dirspell
+  shopt -s globstar
+fi
