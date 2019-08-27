9f2f7ecced5738f354a2e71da15d18c06a235453
Joshua Spence <josh@joshuaspence.com>
2019-08-27 14:10:25 +1000

    Use GNU `coreutils`
    The `ls` available on Mac OS doesn't support `--human-readable`.

--- src/environment/brew.sh
+++ src/environment/brew.sh
@@ -1,11 +1,18 @@
-if test -d /usr/local/opt/findutils/libexec/gnubin; then
+# coreutils
+if test -d /usr/local/opt/coreutils; then
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+fi
+
+# findutils
+if test -d /usr/local/opt/findutils; then
   export PATH="/usr/local/opt/findutils/libexec/gnubin:${PATH}"
 fi
 
+# gnu-getopt
 if test -d /usr/local/opt/gnu-getopt; then
   export PATH="/usr/local/opt/gnu-getopt/bin:${PATH}"
 fi
 
-if test -d /usr/local/opt/python/libexec; then
+if test -d /usr/local/opt/python; then
   export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
 fi
