fb2cc837a1ea612c1d6b61ae7436a1d00dcdafbf
Joshua Spence <josh@joshuaspence.com>
2019-08-29 15:18:43 +1000

    Add a guard around `cloudtoken` configuration

--- src/setup/cloudtoken.sh
+++ src/setup/cloudtoken.sh
@@ -1,6 +1,9 @@
-if ! test -d "${HOME}/.config/cloudtoken"; then
-  cloudtoken --init
-fi
+if command -v cloudtoken &>/dev/null; then
 
-# TODO: This doesn't belong here.
-source "${HOME}/.config/cloudtoken/bashrc_additions"
+  if ! test -d "${HOME}/.config/cloudtoken"; then
+    cloudtoken --init
+  fi
+
+  # TODO: This doesn't belong here.
+  source "${HOME}/.config/cloudtoken/bashrc_additions"
+fi
