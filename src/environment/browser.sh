187b0482b7c6bca5a5a52f83e19a540a4ac21a2f
Joshua Spence <josh@joshuaspence.com>
2019-08-29 14:55:26 +1000

    Set `$BROWSER` on WSL

--- src/environment/browser.sh
+++ src/environment/browser.sh
@@ -4,6 +4,8 @@ elif test -n "${DISPLAY}" && command -v firefox &>/dev/null; then
   export BROWSER='firefox'
 elif command -v lynx &>/dev/null; then
   export BROWSER='lynx'
+elif test -x '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'; then
+  export BROWSER='/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
 else
   # TODO: Should we just always use `open`?
   export BROWSER='open'
