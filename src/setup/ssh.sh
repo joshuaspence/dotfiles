64e774a18da04913b6d576dfffca8417ba16fb8a
Joshua Spence <josh@joshuaspence.com>
2019-08-29 14:58:54 +1000

    Add a TODO comment

--- src/setup/ssh.sh
+++ src/setup/ssh.sh
@@ -7,6 +7,7 @@ fi
 # Run `ssh-agent` if it's not already running.
 # TODO: The agent will terminate with the last Bash session, see
 # https://www.daveeddy.com/2017/10/18/persistent-sshagent-on-bash-on-ubuntu-on-windows/.
+# TODO: Test if `ssh-agent` is actually running with `ssh-add -l`.
 if [[ -v SSH_AGENT_PID ]]; then
   eval "$(ssh-agent -s)"
 fi
