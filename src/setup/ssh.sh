17602a2df65693f5321dc3c6a8d61371df3cf32d
Joshua Spence <josh@joshuaspence.com>
2019-08-29 14:08:01 +1000

    Ensure that SSH agent is running

--- src/setup/ssh.sh
+++ src/setup/ssh.sh
@@ -3,3 +3,10 @@
 if ! test -d "${HOME}/.ssh/connections"; then
   mkdir --mode 700 "${HOME}/.ssh/connections"
 fi
+
+# Run `ssh-agent` if it's not already running.
+# TODO: The agent will terminate with the last Bash session, see
+# https://www.daveeddy.com/2017/10/18/persistent-sshagent-on-bash-on-ubuntu-on-windows/.
+if [[ -v SSH_AGENT_PID ]]; then
+  eval "$(ssh-agent -s)"
+fi
