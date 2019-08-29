769110d6d0cc44c11fe054356c1cc0b7339fd54f
Joshua Spence <josh@joshuaspence.com>
2019-08-29 15:19:12 +1000

    Lint fixes

--- src/functions/wsl.sh
+++ src/functions/wsl.sh
@@ -1,9 +1,9 @@
 # TODO: These functions should only be defined when using WSL.
 
 function win_desktop() {
-  wslpath $(powershell.exe -c "Write-Host ([Environment]::GetFolderPath('Desktop')) -NoNewLine")
+  wslpath "$(powershell.exe -c "Write-Host ([Environment]::GetFolderPath('Desktop')) -NoNewLine")"
 }
 
 function win_mydocuments() {
-  wslpath $(powershell.exe -c "Write-Host ([Environment]::GetFolderPath('MyDocuments')) -NoNewLine")
+  wslpath "$(powershell.exe -c "Write-Host ([Environment]::GetFolderPath('MyDocuments')) -NoNewLine")"
 }
