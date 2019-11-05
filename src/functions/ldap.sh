66abef2290746dbc21ee24fe02970c7acf753bf3
Joshua Spence <josh@joshuaspence.com>
2019-11-05 13:53:10 +1100

    Fix lint

--- src/functions/ldap.sh
+++ src/functions/ldap.sh
@@ -1,1 +1,3 @@
-alias ldapsearch='ldapsearch -o ldif-wrap=no -LLL -x -w $(awk '"'"'$1 == "BINDPW" { print $2 }'"'"' ~/.ldaprc)'
+function ldapsearch() {
+  command ldapsearch -o ldif-wrap=no -LLL -x -w "$(awk '$1 == "BINDPW" { print $2 }' ~/.ldaprc)" "$@"
+}
