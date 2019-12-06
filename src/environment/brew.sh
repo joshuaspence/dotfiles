64269fc82295f501f594499bacfa4f722f3284dc
Joshua Spence <josh@joshuaspence.com>
2019-12-06 15:41:30 +1100

    Revert "Don't hardcode Brew prefix"
    This reverts commit 679532f79209f88fbeb5a51a56f4faddfb3ae26d.

--- src/environment/brew.sh
+++ src/environment/brew.sh
@@ -2,29 +2,27 @@ if [[ $(uname) == "Darwin" && ${BASH_VERSINFO[0]} -lt 4 ]]; then
   echo "bash ${BASH_VERSION} detected. Recommend upgrading to a later version." >&2
 fi
 
-BREW_PREFIX=$(brew --prefix)
-
 # coreutils
-if test -d "${BREW_PREFIX}/opt/coreutils"; then
-  export PATH="${BREW_PREFIX}/opt/coreutils/libexec/gnubin:${PATH}"
+if test -d /usr/local/opt/coreutils; then
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
 fi
 
 # findutils
-if test -d "${BREW_PREFIX}/opt/findutils"; then
-  export PATH="${BREW_PREFIX}/opt/findutils/libexec/gnubin:${PATH}"
+if test -d /usr/local/opt/findutils; then
+  export PATH="/usr/local/opt/findutils/libexec/gnubin:${PATH}"
 fi
 
 # gnu-getopt
-if test -d "${BREW_PREFIX}/opt/gnu-getopt"; then
-  export PATH="${BREW_PREFIX}/opt/gnu-getopt/bin:${PATH}"
+if test -d /usr/local/opt/gnu-getopt; then
+  export PATH="/usr/local/opt/gnu-getopt/bin:${PATH}"
 fi
 
 # libpq
-if test -d "${BREW_PREFIX}/opt/libpq"; then
-  export PATH="${BREW_PREFIX}/opt/libpq/bin:${PATH}"
+if test -d /usr/local/opt/libpq; then
+  export PATH="/usr/local/opt/libpq/bin:${PATH}"
 fi
 
 # python
-if test -d "${BREW_PREFIX}/opt/python"; then
-  export PATH="${BREW_PREFIX}/opt/python/libexec/bin:${PATH}"
+if test -d /usr/local/opt/python; then
+  export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
 fi
