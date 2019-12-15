379e5f61a7c96a626b21f6e869526827fa9071e4
Joshua Spence <josh@joshuaspence.com>
2019-12-15 18:45:50 +1100

    Fix lint

--- test/helper.bash
+++ test/helper.bash
@@ -22,10 +22,11 @@ load 'tools/bats-support/load.bash'
 load 'tools/bats-mock/stub.bash'
 
 function mock_commands() {
+  # shellcheck disable=SC1091
   source /dev/stdin <<EOF
 function command() {
   case \$2 in
-    $(echo $@ | xargs --no-run-if-empty printf '%s) return 0;;\n    ')*) return 1;;
+    $(echo "$@" | xargs --no-run-if-empty printf '%s) return 0;;\n    ')*) return 1;;
   esac
 }
 EOF
