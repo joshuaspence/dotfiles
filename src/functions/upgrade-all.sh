70b30609c1f68751280c5b044244f741b116d0e1
Joshua Spence <josh@joshuaspence.com>
2020-03-05 12:37:07 +1100

    Add `atlas upgrade` to `upgrade-all`

--- src/functions/upgrade-all.sh
+++ src/functions/upgrade-all.sh
@@ -1,7 +1,12 @@
 function upgrade-all() {
+  # Aptitude
   for COMMAND in update upgrade dist-upgrade autoremove autoclean; do
     sudo apt-get "${COMMAND}"
   done
 
+  # Snap
   sudo snap refresh
+
+  # Atlas CLI
+  atlas upgrade
 }
