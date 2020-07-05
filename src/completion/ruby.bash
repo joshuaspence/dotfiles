ae9f1e28174ff00c205b057485cc47c5c6a03b82
Joshua Spence <josh@joshuaspence.com>
2020-07-05 12:35:21 +1000

    Remove Ruby completion

--- src/completion/ruby.bash
+++
@@ -1,5 +0,0 @@
-include completion/bash-completion.bash
-include modules/completion-ruby/completion-bundle
-include modules/completion-ruby/completion-gem
-include modules/completion-ruby/completion-rake
-include modules/completion-ruby/completion-ruby
