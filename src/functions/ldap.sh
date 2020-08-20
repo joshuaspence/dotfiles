25e5d2ecb78423b1bfd765bab59559e28b7a5da3
Joshua Spence <josh@joshuaspence.com>
2020-08-20 11:50:47 +1000

    Base64 encoding for LDAP

--- src/functions/ldap.sh
+++ src/functions/ldap.sh
@@ -1,7 +1,7 @@
 function ldapsearch() {
-  command ldapsearch -o ldif-wrap=no -LLL -x -w "$(awk '$1 == "BINDPW" { print $2 }' ~/.ldaprc)" "$@"
+  command ldapsearch -o ldif-wrap=no -LLL -x -w "$(awk '$1 == "BINDPW" { print $2 }' ~/.ldaprc | base64 --decode)" "$@"
 }
 
 function ldapwhoami() {
-  command ldapwhoami -x -w "$(awk '$1 == "BINDPW" { print $2 }' ~/.ldaprc)" "$@"
+  command ldapwhoami -x -w "$(awk '$1 == "BINDPW" { print $2 }' ~/.ldaprc | base64 --decode)" "$@"
 }
