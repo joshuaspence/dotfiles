96a184bff1c187989f2494f46781fd31b8052d60
Joshua Spence <josh@joshuaspence.com>
2021-04-28 12:08:36 +1000

    Upgrade Cloudtoken

--- src/setup/cloudtoken.sh
+++
@@ -1,9 +0,0 @@
-if command -v cloudtoken &>/dev/null; then
-
-  if ! test -d "${HOME}/.config/cloudtoken"; then
-    cloudtoken --init
-  fi
-
-  # TODO: This doesn't belong here.
-  source "${HOME}/.config/cloudtoken/bashrc_additions"
-fi
