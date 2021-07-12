1f84c765a84a7eea4aa7b16cc7d98bc645ca6036
Joshua Spence <josh@joshuaspence.com>
2021-07-13 09:44:53 +1000

    Add a TODO comment

--- src/completion/hass-cli.bash
+++ src/completion/hass-cli.bash
@@ -1,5 +1,6 @@
 include environment/virtualenv.sh
 
-if command -v hass-cli &>/dev/null; then
-  source <(hass-cli completion bash)
-fi
+# TODO: Uncomment this after https://github.com/home-assistant-ecosystem/home-assistant-cli/issues/367.
+#if command -v hass-cli &>/dev/null; then
+#  source <(hass-cli completion bash)
+#fi
