919f98fef4644bd26706b710263f21bcb4474a4f
Joshua Spence <josh@joshuaspence.com>
2021-08-09 11:25:25 +1000

    Don't override `load` for BATS

--- test/helper.bash
+++ test/helper.bash
@@ -1,22 +1,3 @@
-# Taken from https://stackoverflow.com/a/18839557.
-function copy_function() {
-  test -n "$(declare -f "$1")" || return
-  eval "${_/$1/$2}"
-}
-
-# Taken from https://stackoverflow.com/a/18839557.
-function rename_function() {
-  copy_function "$@" || return
-  unset -f "$1"
-}
-
-# Overload `load` to source a file relative to the repository root.
-rename_function load _load
-
-function load() {
-  _load "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/../${1}"
-}
-
 load 'tools/bats-assert/load.bash'
 load 'tools/bats-support/load.bash'
 load 'tools/bats-mock/stub.bash'
