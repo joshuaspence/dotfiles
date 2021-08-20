50a5a1e072c8a628c24c7853f2c184d07f4fea55
Joshua Spence <josh@joshuaspence.com>
2021-08-20 13:13:22 +1000

    Update `pyenv` script

--- src/environment/pyenv.sh
+++ src/environment/pyenv.sh
@@ -1,7 +1,5 @@
 if test -d "${HOME}/.pyenv"; then
   export PATH="${HOME}/.pyenv/bin:${PATH}"
-
-  # TODO: Should these lines be in `~/.bashrc`?
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
 fi
