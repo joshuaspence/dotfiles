0d1217ba21f8dea477bf31d8855702a8dc2cae68
Joshua Spence <josh@joshuaspence.com>
2022-02-03 15:34:33 +1100

    Enable paginated results from `ldpasearch`

--- src/functions/ldap.sh
+++ src/functions/ldap.sh
@@ -1,5 +1,5 @@
 function ldapsearch() {
-  command ldapsearch -o ldif-wrap=no -LLL -x -w "$(awk '$1 == "BINDPW" { print $2 }' ~/.ldaprc | base64 --decode)" "$@"
+  command ldapsearch -o ldif-wrap=no -LLL -x -w "$(awk '$1 == "BINDPW" { print $2 }' ~/.ldaprc | base64 --decode)" -E pr=1000/noprompt "$@"
 }
 
 function ldapwhoami() {
