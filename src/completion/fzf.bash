9c3f51cc2a1e1d76c504f9921bbcfc7104e49f84
Joshua Spence <josh@joshuaspence.com>
2022-02-14 14:27:39 +1100

    Fix `fzf` completion

---
+++ src/completion/fzf.bash
@@ -0,0 +1,3 @@
+if command -v fzf &>/dev/null; then
+  source /usr/share/doc/fzf/examples/completion.bash
+fi
--- src/config/fzf.bash
+++ src/config/fzf.bash
@@ -1,4 +1,3 @@
 if command -v fzf &>/dev/null; then
   source /usr/share/doc/fzf/examples/key-bindings.bash
-  source /usr/share/doc/fzf/examples/completion.bash
 fi
