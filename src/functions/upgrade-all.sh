a98416f74f9857e3bef93f106784235c9230328a
Joshua Spence <josh@spence.com.au>
2022-02-25 12:22:55 +1100

    Improve upgrade functions

--- src/functions/upgrade-all.sh
+++ src/functions/upgrade-all.sh
@@ -10,7 +10,33 @@ function upgrade-all() {
   # Firmware
   fwupdmgr refresh --force
   fwupdmgr update
+}
 
-  # Atlas CLI
-  atlas upgrade
+function upgrade-atlassian-tools() {
+  upgrade-atlas-cli; atlas upgrade
+  upgrade-cloudtoken
+  upgrade-sourcegraph-cli
+}
+
+function upgrade-atlas-cli() {
+  atlas statlas get --namespace atlas-cli --subdirectory linux/atlas-latest-linux-amd64.tar.gz | tar --extract --gzip --directory ~/bin atlas
+  echo "Atlas CLI upgraded to version $(atlas version)"
+}
+
+function upgrade-cloudtoken() {
+  atlas statlas get --namespace cloudtoken --subdirectory cloudtoken-linux-amd64-latest.zip --output /tmp/cloudtoken.zip
+  unzip -d ~/bin -o /tmp/cloudtoken.zip
+  rm /tmp/cloudtoken.zip
+  echo "Cloudtoken upgraded to version $(cloudtoken --version)"
+}
+
+function upgrade-sourcegraph-cli() {
+  local -r github_repo='sourcegraph/src-cli'
+  local -r latest_release=$(gh --repo "${github_repo}" release list --limit 1 | cut --fields 1)
+  local -r pattern="src-cli_${latest_release}_linux_amd64.tar.gz"
+
+  gh --repo "${github_repo}" release download "${latest_release}" --dir /tmp --pattern "${pattern}"
+  tar --extract --file "/tmp/${pattern}" --directory ~/bin
+  rm "/tmp/${pattern}"
+  src version
 }
