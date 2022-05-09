63729e51dace193afa34b4c1fe4fd97edec5c358
Joshua Spence <josh@spence.com.au>
2022-05-10 08:50:50 +1000

    Add `--no-run-if-empty` to `xargs`

--- src/functions/docker-clean-all.sh
+++ src/functions/docker-clean-all.sh
@@ -1,4 +1,4 @@
 function docker-clean-all() {
-  docker ps --all --quiet | xargs docker rm --force
-  docker images --all --quiet | xargs docker rmi --force
+  docker ps --all --quiet | xargs --no-run-if-empty docker rm --force
+  docker images --all --quiet | xargs --no-run-if-empty docker rmi --force
 }
