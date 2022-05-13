4e4422e1e993b5639f2d550bf791a6ce64b66dbb
Joshua Spence <josh@spence.com.au>
2022-05-13 14:58:03 +1000

    Add a TODO comment

--- src/functions/docker-clean-all.sh
+++ src/functions/docker-clean-all.sh
@@ -1,3 +1,4 @@
+# TODO: Clean volumes and networks.
 function docker-clean-all() {
   docker ps --all --quiet | xargs --no-run-if-empty docker rm --force
   docker images --all --quiet | xargs --no-run-if-empty docker rmi --force
