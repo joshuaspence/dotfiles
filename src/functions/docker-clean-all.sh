1160b9a8c31fa745d0a0a845195f5b3bd5bb1644
Joshua Spence <josh@spence.com.au>
2022-05-13 15:00:26 +1000

    Prune Docker networks and volumes

--- src/functions/docker-clean-all.sh
+++ src/functions/docker-clean-all.sh
@@ -1,5 +1,6 @@
-# TODO: Clean volumes and networks.
 function docker-clean-all() {
   docker ps --all --quiet | xargs --no-run-if-empty docker rm --force
-  docker images --all --quiet | xargs --no-run-if-empty docker rmi --force
+  docker image prune --all --force
+  docker network prune --force
+  docker volume prune --force
 }
