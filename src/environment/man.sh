b6575fb4bbfba30220a25e8ca916dfed445cbc93
Joshua Spence <josh@spence.com.au>
2022-07-06 10:01:37 +1000

    Add `bat-extras`

--- src/environment/man.sh
+++
@@ -1,3 +0,0 @@
-if command -v batcat &>/dev/null; then
-  export MANPAGER="sh -c 'col --no-backspaces --spaces | batcat --plain --language=man'"
-fi
--- src/functions/bat.sh
+++ src/functions/bat.sh
@@ -1,2 +1,8 @@
-alias bat='batcat'
-alias cat='batcat --paging=never'
+if command -v batcat &>/dev/null; then
+  alias bat='batcat'
+  alias batdiff="${HOME}/dotfiles/src/modules/bat-extras/src/batdiff.sh"
+  alias batman="${HOME}/dotfiles/src/modules/bat-extras/src/batman.sh"
+  alias cat='batcat --paging=never'
+
+  source <("${HOME}/dotfiles/src/modules/bat-extras/src/batpipe.sh")
+fi
