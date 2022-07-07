e1e4541222059507e61dc0bdaeff9825be256858
Joshua Spence <josh@spence.com.au>
2022-07-07 19:24:54 +1000

    Simplify file inclusion

--- src/functions/include.sh
+++
@@ -1,6 +0,0 @@
-# TODO: Use globs in bash.
-function _dotfiles_include() {
-  while IFS= read -r -d '' FILE; do
-    . "${FILE}"
-  done < <(find ~/dotfiles/"$1" -regex ".*\.\($(basename "$0")\|sh\)$" -print0)
-}
