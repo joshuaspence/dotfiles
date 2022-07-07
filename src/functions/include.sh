7a70c0ac0f6b61ce5d3e8f4cdfd852cf0fe5cb38
Joshua Spence <josh@spence.com.au>
2022-07-07 15:22:23 +1000

    Fix shell detection in `_dotfiles_include`

--- src/functions/include.sh
+++ src/functions/include.sh
@@ -2,5 +2,5 @@
 function _dotfiles_include() {
   while IFS= read -r -d '' FILE; do
     . "${FILE}"
-  done < <(find ~/dotfiles/"$1" -regex ".*\.\($0\|sh\)$" -print0)
+  done < <(find ~/dotfiles/"$1" -regex ".*\.\($(basename "$0")\|sh\)$" -print0)
 }
