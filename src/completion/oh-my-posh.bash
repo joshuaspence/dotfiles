5af96931c4492472a4c63f01eea3e572008716c7
Joshua Spence <josh@spence.com.au>
2022-07-10 19:48:07 +1000

    Revert "Add `oh-my-posh`"
    This reverts commit c016ec0187ad431b7f41f140783aaecd860823b3.

--- src/completion/oh-my-posh.bash
+++
@@ -1,3 +0,0 @@
-if command -v oh-my-posh &>/dev/null; then
-  source <(oh-my-posh completion bash)
-fi
--- src/config/oh-my-posh.bash
+++
@@ -1,5 +0,0 @@
-if command -v oh-my-posh &>/dev/null; then
-  source <(oh-my-posh init bash --config ~/.omp.yaml)
-
-  # TODO: Install fonts with `oh-my-posh font install Meslo`
-fi
