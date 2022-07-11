2e6e8bafd5fe2030f0b0ade266b2dd98e5cee035
Joshua Spence <josh@spence.com.au>
2022-07-11 12:28:57 +1000

    Move `bat-extras`

--- src/functions/bat.sh
+++ src/functions/bat.sh
@@ -1,8 +1,8 @@
 # shellcheck disable=SC2139
 if command -v batcat &>/dev/null; then
   alias bat='batcat'
-  alias batdiff="${HOME}/dotfiles/src/modules/bat-extras/src/batdiff.sh"
-  alias batman="${HOME}/dotfiles/src/modules/bat-extras/src/batman.sh"
+  alias batdiff="${HOME}/dotfiles/tools/bat-extras/src/batdiff.sh"
+  alias batman="${HOME}/dotfiles/tools/bat-extras/src/batman.sh"
   alias cat='batcat --paging=never'
 
   source <("${HOME}/dotfiles/src/modules/bat-extras/src/batpipe.sh")
