1db27ba4f0b2d35a2ddb8e0c6880c23ed050ea60
Joshua Spence <josh@spence.com.au>
2022-07-12 11:08:40 +1000

    Allow `vscode` function to accept a directory

--- src/functions/vscode.sh
+++ src/functions/vscode.sh
@@ -1,7 +1,9 @@
 function vscode() {
+  test -n "${1}" && cd "${1}"
+
   if test -d .devcontainer; then
-    devcontainer open-in-code . "$@"
+    devcontainer open-in-code . "${@:1}"
   else
-    code . "$@"
+    code . "${@:1}"
   fi
 }
