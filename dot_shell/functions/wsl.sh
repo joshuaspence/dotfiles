17148ef29985eee9d96eaeee4c02d49245c120fa
Joshua Spence <josh@spence.com.au>
2022-07-14 10:12:49 +1000

    Remove WSL functions

--- dot_shell/functions/wsl.sh
+++
@@ -1,9 +0,0 @@
-# TODO: These functions should only be defined when using WSL.
-
-function win_desktop() {
-  wslpath "$(powershell.exe -c "Write-Host ([Environment]::GetFolderPath('Desktop')) -NoNewLine")"
-}
-
-function win_mydocuments() {
-  wslpath "$(powershell.exe -c "Write-Host ([Environment]::GetFolderPath('MyDocuments')) -NoNewLine")"
-}
