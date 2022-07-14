31d7b2a8384f9d7babbccc4f633b9ec77bb3361e
Joshua Spence <josh@spence.com.au>
2022-07-14 15:33:12 +1000

    Remove `bat-extras`

--- dot_shell/functions/bat.sh
+++ dot_shell/functions/bat.sh
@@ -1,9 +1,5 @@
 # shellcheck disable=SC2139
 if command -v batcat &>/dev/null; then
   alias bat='batcat'
-  alias batdiff="${HOME}/.shell/modules/bat-extras/src/batdiff.sh"
-  alias batman="${HOME}/.shell/modules/bat-extras/src/batman.sh"
   alias cat='batcat --paging=never'
-
-  source <("${HOME}/.shell/modules/bat-extras/src/batpipe.sh")
 fi
