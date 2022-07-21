1f543de8fc58e1f45ba26bc8da9d29b25b76d9ee
Joshua Spence <josh@spence.com.au>
2022-07-21 22:02:54 +1000

    Minor

--- shell/functions/ls.sh
+++ shell/functions/ls.sh
@@ -1,7 +1,5 @@
-# Enable colored output for `ls`.
 if command -v dircolors >/dev/null; then
   source <(dircolors)
-  alias ls='ls --color=auto --human-readable'
-else
-  alias ls='ls --human-readable'
 fi
+
+alias ls='ls --color=auto --human-readable'
