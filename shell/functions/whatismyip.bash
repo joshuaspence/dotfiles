79b5446f846594767a72309025236a779624a58c
Joshua Spence <josh@spence.com.au>
2022-07-22 13:27:27 +1000

    Tweak `whatismyip` functions

--- shell/functions/whatismyip.bash
+++
@@ -1,7 +0,0 @@
-function whatismyipv4() {
-  dig -4 -t A myip.opendns.com @resolver1.opendns.com +short
-}
-
-function whatismyipv6() {
-  dig -6 -t AAAA myip.opendns.com @resolver1.opendns.com +short
-}
