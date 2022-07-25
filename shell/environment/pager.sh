cc5e7b3f2359fc1c11c5ff888a1bdaf6a6fbbba8
Joshua Spence <josh@spence.com.au>
2022-07-25 10:58:25 +1000

    Add `~/.lesskey`

--- shell/environment/pager.sh
+++ shell/environment/pager.sh
@@ -1,11 +1,9 @@
 if command -v less >/dev/null; then
-  export LESS='--ignore-case --RAW-CONTROL-CHARS --chop-long-lines --tabs=4'
+  if command -v lesspipe >/dev/null; then
+    source <(lesspipe)
+  fi
+
   export PAGER='less'
 elif command -v more >/dev/null; then
   export PAGER='more'
-else
-  unset PAGER
-
-  # shellcheck disable=SC2016
-  echo 'No command set for $PAGER' >&2
 fi
