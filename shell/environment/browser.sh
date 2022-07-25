af13336917deae0d2086081fb39a430310dce7cd
Joshua Spence <josh@spence.com.au>
2022-07-25 11:08:14 +1000

    Simplify `$BROWSER` logic

--- shell/environment/browser.sh
+++ shell/environment/browser.sh
@@ -1,12 +1,3 @@
-if test -n "${DISPLAY}" && command -v google-chrome >/dev/null; then
+if test -v DISPLAY && command -v google-chrome >/dev/null; then
   export BROWSER='google-chrome'
-elif test -n "${DISPLAY}" && command -v firefox >/dev/null; then
-  export BROWSER='firefox'
-elif command -v lynx >/dev/null; then
-  export BROWSER='lynx'
-elif test -x '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'; then
-  export BROWSER='/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
-else
-  # TODO: Should we just always use `open`?
-  export BROWSER='open'
 fi
