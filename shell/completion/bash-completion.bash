0689c8972e80878d940e0006da65effa291c1929
Joshua Spence <josh@spence.com.au>
2022-07-26 13:16:13 +1000

    Minor

--- shell/completion/bash-completion.bash
+++ shell/completion/bash-completion.bash
@@ -1,5 +1,4 @@
 # Load completions provided by the `bash-completion` package.
-
 if test -f /usr/share/bash-completion/bash_completion; then
   source /usr/share/bash-completion/bash_completion
 elif test -f /etc/bash_completion; then
@@ -7,4 +6,5 @@ elif test -f /etc/bash_completion; then
 fi
 
 # Prevent `~` from being expanded to `$HOME`.
+# See https://superuser.com/a/442767.
 function _expand() { :; }
