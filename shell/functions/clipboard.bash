368321b50bf990b8f69f0f0fb291a5df8675c604
Joshua Spence <josh@spence.com.au>
2022-07-26 13:04:42 +1000

    Appease `shfmt`

--- shell/functions/clipboard.bash
+++ shell/functions/clipboard.bash
@@ -1,7 +1,7 @@
 function clipboard() {
   # If standard input is a pipe then forward the stream to the clipboard.
   if ! test -t 0; then
-    xclip -in -selection clipboard < /dev/stdin
+    xclip -in -selection clipboard </dev/stdin
   fi
 
   xclip -out -selection clipboard
