d93cdb37e80d5ea5a5f7d203b1f316e8c6e64837
Joshua Spence <josh@spence.com.au>
2022-07-28 10:52:06 +1000

    Remove empty script

--- shell/environment/ssh.sh
+++
@@ -1,7 +0,0 @@
-# Run `ssh-agent` if it's not already running.
-# TODO: The agent will terminate with the last Bash session, see
-# https://www.daveeddy.com/2017/10/18/persistent-sshagent-on-bash-on-ubuntu-on-windows/.
-# TODO: Test if `ssh-agent` is actually running with `ssh-add -l`.
-if command -v ssh-agent >/dev/null && ! test -v SSH_AGENT_PID; then
-  echo "ssh-agent doesn't appear to be running"
-fi
