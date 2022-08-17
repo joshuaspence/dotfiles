b2cda886cd09c773cbe5e17a934f8f5888e678b5
Joshua Spence <josh@spence.com.au>
2022-08-17 15:52:58 +1000

    Move completion to `~/.bash_completion`

--- shell/completion/bash-completion.bash
+++ shell/completion/bash-completion.bash
@@ -1,10 +1,3 @@
-# Load completions provided by the `bash-completion` package.
-if test -f /usr/share/bash-completion/bash_completion; then
-  source /usr/share/bash-completion/bash_completion
-elif test -f /etc/bash_completion; then
-  source /etc/bash_completion
-fi
-
 # Prevent `~` from being expanded to `$HOME`.
 # See https://superuser.com/a/442767.
 function _expand() { :; }
