62513cad82535f4e96d10fb8f8e521064870052f
Joshua Spence <josh@spence.com.au>
2022-08-17 13:19:54 +1000

    Install `yq` from package

--- shell/completion/yq.bash
+++ shell/completion/yq.bash
@@ -1,3 +1,7 @@
 if command -v yq >/dev/null; then
+  if complete -p yq &>/dev/null; then
+    echo 'Readline completion has already been setup for `yq`'. >&2
+  fi
+
   source <(yq shell-completion bash)
 fi
