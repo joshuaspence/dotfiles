abae3814b57288c0f276bed5753d9fd27d0c866b
Joshua Spence <josh@spence.com.au>
2022-08-17 16:06:10 +1000

    Lint fix

--- shell/completion/yq.bash
+++ shell/completion/yq.bash
@@ -1,6 +1,6 @@
 if command -v yq >/dev/null; then
   if complete -p yq &>/dev/null; then
-    echo 'Readline completion has already been setup for `yq`'. >&2
+    echo "Readline completion has already been setup for \`yq\`". >&2
   fi
 
   source <(yq shell-completion bash)
