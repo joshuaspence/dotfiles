c7135a3d127047f1e07ca3183f70448e8d21e4c7
Joshua Spence <josh@spence.com.au>
2022-08-18 10:58:46 +1000

    Set `$XDG_RUNTIME_DIR`

--- shell/environment/xdg.sh
+++ shell/environment/xdg.sh
@@ -2,5 +2,6 @@ XDG_CACHE_HOME="${HOME}/.cache"
 XDG_CONFIG_HOME="${HOME}/.config"
 XDG_DATA_HOME="${HOME}/.local/share"
 XDG_STATE_HOME="${HOME}/.local/state"
+XDG_RUNTIME_DIR="/run/user/$(id --user)"
 
 export WGETRC="${XDG_CONFIG_HOME}/wgetrc"
