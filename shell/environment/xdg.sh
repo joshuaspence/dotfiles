84c6abd11990a86f84938fb51195364937d6d42e
Joshua Spence <josh@spence.com.au>
2022-08-18 11:28:10 +1000

    Move `~/.inputrc` to `$XDG_CONFIG_HOME`

--- shell/environment/xdg.sh
+++ shell/environment/xdg.sh
@@ -4,5 +4,6 @@ XDG_DATA_HOME="${HOME}/.local/share"
 XDG_STATE_HOME="${HOME}/.local/state"
 XDG_RUNTIME_DIR="/run/user/$(id --user)"
 
+export INPUTRC="${XDG_CONFIG_HOME}/readline/inputrc"
 export NODE_REPL_HISTORY="${XDG_DATA_HOME}/node_repl_history"
 export WGETRC="${XDG_CONFIG_HOME}/wgetrc"
