544592f120993086761ac78137dfa3c3e43d3284
Joshua Spence <josh@spence.com.au>
2022-08-18 12:38:26 +1000

    Fix lint

--- shell/environment/xdg.sh
+++ shell/environment/xdg.sh
@@ -1,9 +1,11 @@
 # shellcheck disable=SC2034
-XDG_CACHE_HOME="${HOME}/.cache"
-XDG_CONFIG_HOME="${HOME}/.config"
-XDG_DATA_HOME="${HOME}/.local/share"
-XDG_STATE_HOME="${HOME}/.local/state"
-XDG_RUNTIME_DIR="/run/user/$(id --user)"
+{
+  XDG_CACHE_HOME="${HOME}/.cache"
+  XDG_CONFIG_HOME="${HOME}/.config"
+  XDG_DATA_HOME="${HOME}/.local/share"
+  XDG_STATE_HOME="${HOME}/.local/state"
+  XDG_RUNTIME_DIR="/run/user/$(id --user)"
+}
 
 export INPUTRC="${XDG_CONFIG_HOME}/readline/inputrc"
 export NODE_REPL_HISTORY="${XDG_DATA_HOME}/node_repl_history"
