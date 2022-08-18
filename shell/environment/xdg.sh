558277031f00f4a6589f15ca153a89f22f151d3e
Joshua Spence <josh@spence.com.au>
2022-08-18 11:26:23 +1000

    Move `~/.node_repl_history` to `$XDG_DATA_HOME`

--- shell/environment/xdg.sh
+++ shell/environment/xdg.sh
@@ -4,4 +4,5 @@ XDG_DATA_HOME="${HOME}/.local/share"
 XDG_STATE_HOME="${HOME}/.local/state"
 XDG_RUNTIME_DIR="/run/user/$(id --user)"
 
+export NODE_REPL_HISTORY="${XDG_DATA_HOME}/node_repl_history"
 export WGETRC="${XDG_CONFIG_HOME}/wgetrc"
