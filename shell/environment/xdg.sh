b17074f51b3c395691a85965c21576099ac187d8
Joshua Spence <josh@spence.com.au>
2022-08-18 10:22:20 +1000

    Revert "Revert "Move `~/.wgetrc` to `$XDG_CONFIG_HOME`""
    This reverts commit 46929336b10b69a1f49b2a2f6e5b59cfbd576348.

---
+++ shell/environment/xdg.sh
@@ -0,0 +1,6 @@
+XDG_CACHE_HOME="${HOME}/.cache"
+XDG_CONFIG_HOME="${HOME}/.config"
+XDG_DATA_HOME="${HOME}/.local/share"
+XDG_STATE_HOME="${HOME}/.local/state"
+
+export WGETRC="${XDG_CONFIG_HOME}/wgetrc"
