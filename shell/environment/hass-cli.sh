78102009c1eafd3f3b5dfa74e1ea49545e4677e4
Joshua Spence <josh@spence.com.au>
2022-08-31 11:55:06 +1000

    Set `HASS_SERVER` and `HASS_TOKEN`

--- shell/environment/hass-cli.sh
+++ shell/environment/hass-cli.sh
@@ -1,3 +1,6 @@
-if test -f "${HOME}/.hassrc"; then
-  source "${HOME}/.hassrc"
+# TODO: This should only be included when `.personal` is `true` (see twpayne/chezmoi#2310).
+# TODO: Use the `lastpassRaw` template function (see twpayne/chezmoi#2310).
+if command_exists hass-cli && lpass status --quiet; then
+  export HASS_SERVER='http://homeassistant.spence.network'
+  export HASS_TOKEN="$(lpass show --notes hass-cli)"
 fi
