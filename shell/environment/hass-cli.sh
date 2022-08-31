2fd29f11c2dcb9a26ccb2a9c29a8b767af857d9c
Joshua Spence <josh@spence.com.au>
2022-08-31 12:11:07 +1000

    Fix lint

--- shell/environment/hass-cli.sh
+++ shell/environment/hass-cli.sh
@@ -1,6 +1,7 @@
 # TODO: This should only be included when `.personal` is `true` (see twpayne/chezmoi#2310).
 # TODO: Use the `lastpassRaw` template function (see twpayne/chezmoi#2310).
 if command_exists hass-cli && lpass status --quiet; then
-  export HASS_SERVER='http://homeassistant.spence.network'
-  export HASS_TOKEN="$(lpass show --notes hass-cli)"
+  HASS_SERVER='http://homeassistant.spence.network'
+  HASS_TOKEN="$(lpass show --notes hass-cli)"
+  export HASS_SERVER HASS_TOKEN
 fi
