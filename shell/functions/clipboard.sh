6f5f6e6b09150206d62cb9b384de15bf53f3ee3c
Joshua Spence <josh@spence.com.au>
2022-09-02 12:28:12 +1000

    Make `clipboard` function POSIX compatible

--- shell/functions/clipboard.sh
+++ shell/functions/clipboard.sh
@@ -1,4 +1,4 @@
-function clipboard() {
+clipboard() {
   # If standard input is a pipe then forward the stream to the clipboard.
   if ! test -t 0; then
     xclip -in -selection clipboard </dev/stdin
