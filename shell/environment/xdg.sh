b3d3539f194f77778bd44d81cbd1c837dcb36eb6
Joshua Spence <josh@spence.com.au>
2022-09-02 12:53:49 +1000

    Move AWS config to `$XDG_CONFIG_HOME`

--- shell/environment/xdg.sh
+++ shell/environment/xdg.sh
@@ -17,6 +17,10 @@ export WGETRC="${XDG_CONFIG_HOME}/wgetrc"
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
 export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
 
+# See aws/aws-sdk#30.
+export AWS_CONFIG_FILE="${XDG_CONFIG_HOME}/aws/config"
+export AWS_SHARED_CREDENTIALS_FILE="${XDG_CONFIG_HOME}/aws/credentials"
+
 # `less` gains full support for XDG base directories in version 600 (see gwsw/less#153).
 if test "$(less --version | head -n1 | cut -d' ' -f2)" -le 600; then
   export LESSKEYIN="${XDG_CONFIG_HOME}/lesskey"
