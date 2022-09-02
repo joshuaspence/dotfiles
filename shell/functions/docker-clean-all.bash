8e1a3b6ee540b304a3d591dfc2a6883012b9b0a3
Joshua Spence <josh@spence.com.au>
2022-09-02 12:32:50 +1000

    Remove `docker-clean-all`
    This seems to be equivalent to `docker system prune`, see https://docs.docker.com/config/pruning/#prune-everything.

--- shell/functions/docker-clean-all.bash
+++
@@ -1,6 +0,0 @@
-function docker-clean-all() {
-  docker ps --all --quiet | xargs --no-run-if-empty docker rm --force
-  docker image prune --all --force
-  docker network prune --force
-  docker volume prune --force
-}
