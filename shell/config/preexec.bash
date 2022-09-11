0cccc17d151c8a787f6e5792cfa49a103c6f43b0
Joshua Spence <josh@spence.com.au>
2022-09-12 08:55:23 +1000

    Revert "Fix cycling through history with up/down"
    This reverts commit 1bed25da511eea783f816332ec0004a80a4660c0.

--- shell/config/preexec.bash
+++ shell/config/preexec.bash
@@ -4,19 +4,4 @@ fi
 
 if command_exists atuin; then
   source <(atuin init bash)
-
-  # Pull in changes from ellie/atuin#445 until a new version is released.
-  if [[ $(atuin --version) == 'atuin 0.10.0' ]]; then
-    function __atuin_history() {
-      tput rmkx
-      HISTORY="$(RUST_LOG=error atuin search -i -- "${READLINE_LINE}" 3>&1 1>&2 2>&3)"
-      tput smkx
-
-      READLINE_LINE=${HISTORY}
-      READLINE_POINT=${#READLINE_LINE}
-    }
-
-    bind -x '"\e[A": __atuin_history'
-    bind -x '"\eOA": __atuin_history'
-  fi
 fi
