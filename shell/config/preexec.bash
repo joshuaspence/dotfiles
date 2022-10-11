a2d829f3777ac6b69008f490a8e5580e0c3a6bdb
Joshua Spence <josh@spence.com.au>
2022-10-11 21:17:51 +1100

    Revert "Temporarily disable `atuin`"
    This reverts commit 781ac99edc6d2bfde90f2f238eebe222f739187a.

--- shell/config/preexec.bash
+++ shell/config/preexec.bash
@@ -2,6 +2,6 @@ if test -f ~/.bash-preexec.sh; then
   source ~/.bash-preexec.sh
 fi
 
-#if command_exists atuin; then
-#  source <(atuin init bash)
-#fi
+if command_exists atuin; then
+  source <(atuin init bash)
+fi
