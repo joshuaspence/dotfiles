3376a19588d4737769076b586a429891936ca5b0
Joshua Spence <josh@spence.com.au>
2022-11-01 09:46:49 +1100

    Revert "Install `atuin`"
    This reverts commit 3fdf4923d82b067cd6966b0462d2d2290c546c58.

--- shell/completion/atuin.bash
+++
@@ -1,3 +0,0 @@
-if command_exists atuin; then
-  source <(atuin gen-completions --shell bash)
-fi
--- shell/config/preexec.bash
+++ shell/config/preexec.bash
@@ -1,7 +1,3 @@
 if test -f ~/.bash-preexec.sh; then
   source ~/.bash-preexec.sh
 fi
-
-if command_exists atuin; then
-  source <(atuin init bash)
-fi
