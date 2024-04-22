85f2691fe4c15cb9a6d3a41529ba371d31eef005
Josh Spence <jspence@anduril.com>
2024-04-22 10:54:51 +1000

    Fix bootstrap test

--- src/snap.bash
+++ src/snap.bash
@@ -1,4 +1,5 @@
+# shellcheck disable=SC2032,SC2033
 function snap() {
   local -r name="$1"
-  command snap list "${name}" &>/dev/null || sudo command snap install "${name}"
+  command snap list "${name}" &>/dev/null || sudo snap install "${name}" "${@:2}"
 }
