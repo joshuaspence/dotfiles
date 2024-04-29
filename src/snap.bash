d3f81f5a461680e585be5e297b458a395f03d7a5
Josh Spence <jspence@anduril.com>
2024-04-29 16:18:15 +1000

    Remove unneccessary use of `sudo`

--- src/snap.bash
+++ src/snap.bash
@@ -1,5 +1,4 @@
-# shellcheck disable=SC2032,SC2033
 function snap() {
   local -r name="$1"
-  command snap list "${name}" &>/dev/null || sudo snap install "${name}" "${@:2}"
+  command snap list "${name}" &>/dev/null || command snap install "${name}" "${@:2}"
 }
