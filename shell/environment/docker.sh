f7adaa786ea01cf6520a35870ad63c9b5fb213ae
Josh Spence <jspence@anduril.com>
2024-12-07 13:30:49 +1100

    Fix `DOCKER_ORIG_CONFIG`

--- shell/environment/docker.sh
+++ shell/environment/docker.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=sh
 
 if command_exists docker-credential-magic; then
-  export DOCKER_ORIG_CONFIG="${DOCKER_CONFIG}/magic"
+  export DOCKER_ORIG_CONFIG="${HOME}/.config/magic"
 fi
