0ce3946565fe20334e31a4364d8ffb2fbe979be1
Josh Spence <jspence@anduril.com>
2025-01-17 10:05:28 +1100

    Set `AWS_ECR_IGNORE_CREDS_STORAGE`

--- shell/environment/docker.sh
+++ shell/environment/docker.sh
@@ -3,3 +3,7 @@
 if command_exists docker-credential-magic; then
   export DOCKER_ORIG_CONFIG="${HOME}/.config/magic"
 fi
+
+if command_exists docker-credential-ecr-login; then
+  export AWS_ECR_IGNORE_CREDS_STORAGE=true
+fi
