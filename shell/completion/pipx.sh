e5ff2daa5260945a9996e50be99cfecc17b652ba
Josh Spence <jspence@anduril.com>
2025-01-21 18:05:09 +1100

    Minor

--- shell/completion/pipx.sh
+++ shell/completion/pipx.sh
@@ -1,9 +1,5 @@
 # shellcheck shell=sh
 
 if command_exists pipx; then
-  if command_exists register-python-argcomplete3; then
-    eval "$(register-python-argcomplete3 pipx)"
-  elif command_exists register-python-argcomplete; then
-    eval "$(register-python-argcomplete pipx)"
-  fi
+  eval "$(register-python-argcomplete pipx)"
 fi
