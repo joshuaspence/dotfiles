090ac505b3b6a4029824032f83e52cbdcac88b5a
Josh Spence <jspence@anduril.com>
2025-03-27 12:11:33 +1100

    Run `shfmt`

--- shell/functions/aws.bash
+++ shell/functions/aws.bash
@@ -5,7 +5,7 @@ function aws-sso-refresh-credentials() {
   grep --perl-regexp --only-matching '(?<=^\[sso-session ).+(?=\]$)' "${AWS_CONFIG_FILE}" | while IFS= read -r SSO_SESSION; do
     SSO_SESSION_ID=$(echo -n "${SSO_SESSION}" | sha1sum | cut --delimiter=' ' --fields=1)
 
-    if (( $(date --date "$(jq --raw-output .expiresAt "${AWS_SSO_CACHE_DIR}/${SSO_SESSION_ID}.json" 2>/dev/null)" +%s) > $(date +%s) )); then
+    if (($(date --date "$(jq --raw-output .expiresAt "${AWS_SSO_CACHE_DIR}/${SSO_SESSION_ID}.json" 2>/dev/null)" +%s) > $(date +%s))); then
       # SSO credentials are still valid, refresh is not unnecessary.
       continue
     fi
