31664f8d5ae91c2964896db3dcc9d9dd3390a898
Josh Spence <josh@spence.com.au>
2026-03-11 08:46:12 +1100

    Enable more command completions for `git`

--- shell/config/completion.bash
+++ shell/config/completion.bash
@@ -1,3 +1,6 @@
+GIT_COMPLETION_SHOW_ALL=1
+GIT_COMPLETION_SHOW_ALL_COMMANDS=1
+
 # Load completions provided by the `bash-completion` package.
 if test -f /usr/share/bash-completion/bash_completion; then
   source /usr/share/bash-completion/bash_completion
