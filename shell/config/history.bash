d6eb7f577a85c837bfdeea4bc1baf9a21bb131e4
Josh Spence <josh@spence.com.au>
2026-03-13 09:30:45 +1100

    Make `PROMPT_COMMAND` an array

--- shell/config/history.bash
+++ shell/config/history.bash
@@ -1,14 +1,12 @@
 HISTCONTROL='ignoreboth'
 HISTFILE="${XDG_DATA_HOME:-${HOME}/.local/share}/bash/history"
 HISTFILESIZE=1048576
-HISTSIZE=1048576
-
-# Exclude certain commands from the history file.
 HISTIGNORE='[bf]g:cd *:clear:exit:history:ls:pwd'
+HISTSIZE=1048576
 
 # Whenever displaying the prompt, write the previous line to disk.
 # See https://unix.stackexchange.com/a/48113.
-PROMPT_COMMAND='history -a'
+PROMPT_COMMAND+=('history -a')
 
 shopt -s histappend
 shopt -s histreedit
