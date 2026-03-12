013aa40b4574fea439675dbe4a988b9e9179a3c4
Josh Spence <josh@spence.com.au>
2026-03-13 08:38:40 +1100

    Fallback to Firefox

--- shell/environment/browser.sh
+++ shell/environment/browser.sh
@@ -3,5 +3,7 @@
 if test -n "${DISPLAY+x}"; then
   if command_exists google-chrome; then
     export BROWSER='google-chrome'
+  elif command_exists firefox; then
+    export BROWSER='firefox'
   fi
 fi
