295f1e53a0deac135f32e2143b3fb116ae1e63d0
Josh Spence <josh@spence.com.au>
2026-03-18 17:12:15 +1100

    Minor

--- shell/functions/resize-pdf.bash
+++ shell/functions/resize-pdf.bash
@@ -1,3 +1,8 @@
 function resize-pdf() {
-  gs -dNOPAUSE -dPDFSETTINGS=/default -dQUIET -sDEVICE=pdfwrite -sOutputFile="${2}" "${1}"
+  if (( $# != 2 )); then
+    echo 'Usage: resize-pdf <input.pdf> <output.pdf>' >&2
+    return 1
+  fi
+  
+  gs -dNOPAUSE -dQUIET -sDEVICE=pdfwrite -sOutputFile="$2" "$1"
 }
