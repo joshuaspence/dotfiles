4911baab5048a3f4c9036e75bc88d6dba80316aa
Josh Spence <josh@spence.com.au>
2026-04-17 07:57:00 +1000

    Update some dconf settings

--- .chezmoiscripts/run_onchange_dconf.sh
+++ .chezmoiscripts/run_onchange_dconf.sh
@@ -5,8 +5,27 @@ set -o nounset
 set -o pipefail
 
 dconf load / <<'EOF'
+[org/gnome/desktop/calendar]
+show-weekdate = false
+
 [org/gnome/desktop/datetime]
 automatic-timezone = true
+
+[org/gnome/desktop/interface]
+clock-show-date = true
+clock-show-seconds = false
+clock-show-weekday = false
+show-battery-percentage = true
+
+[org/gnome/desktop/notifications]
+show-in-lock-screen = true
+
+[org/gnome/desktop/privacy]
+old-files-age = 30
+recent-files-max-age = -1
+remember-recent-files = true
+remove-old-temp-files = true
+remove-old-trash-files = true
 
 [org/gnome/meld]
 folder-status-filters = ['normal', 'normal', 'new', 'modified']
@@ -14,26 +33,65 @@ highlight-syntax = true
 indent-width = 2
 
 [org/gnome/mutter]
+attach-modal-dialogs = true
+center-new-windows = true
+dynamic-workspaces = true
 experimental-features = ['x11-randr-fractional-scaling']
 workspaces-only-on-primary = false
+
+[org/gnome/nautilus/window-state]
+maximized = true
 
 [org/gnome/settings-daemon/plugins/media-keys]
 calculator = ['<Primary><Alt>c']
 home = ['<Primary><Alt>e']
+terminal = ['<Primary><Alt>t']
 
 [org/gnome/settings-daemon/plugins/power]
 ambient-enabled = false
 
 [org/gnome/shell]
-enabled-extensions = ['tiling-assistant@leleat-on-github', 'tiling-assistant@ubuntu.com', 'ubuntu-dock@ubuntu.com', 'ubuntu-appindicators@ubuntu.com', 'system-monitor@gnome-shell-extensions.gcampax.github.com', 'auto-move-windows@gnome-shell-extensions.gcampax.github.com', 'nightthemeswitcher@romainvigier.fr']
+enabled-extensions = ['tiling-assistant@leleat-on-github', 'tiling-assistant@ubuntu.com', 'ubuntu-dock@ubuntu.com', 'ubuntu-appindicators@ubuntu.com', 'system-monitor@gnome-shell-extensions.gcampax.github.com', 'auto-move-windows@gnome-shell-extensions.gcampax.github.com', 'nightthemeswitcher@romainvigier.fr', 'ding@rastersoft.com', 'launch-new-instance@gnome-shell-extensions.gcampax.github.com', 'workspace-indicator@gnome-shell-extensions.gcampax.github.com']
 favorite-apps = ['google-chrome.desktop', 'slack_slack.desktop', 'code.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'Zoom.desktop']
 
+[org/gnome/shell/extensions/auto-move-windows]
+application-list = ['slack_slack.desktop:1']
+
 [org/gnome/shell/extensions/dash-to-dock]
+autohide = false
 dock-fixed = false
+dock-position = 'LEFT'
+extend-height = true
 multi-monitor = true
+shift-click-action = 'launch'
+show-apps-at-top = false
+show-favorites = true
+show-running = true
 
 [org/gnome/shell/extensions/nightthemeswitcher/time]
+manual-schedule = false
 time-source = 'location'
+
+[org/gnome/shell/extensions/system-monitor]
+show-cpu = true
+show-download = true
+show-memory = true
+show-swap = true
+show-upload = true
+
+[org/gnome/shell/extensions/tiling-assistant]
+enable-tiling-popup = false
+restore-window = ['<Super>Down']
+tile-bottom-half = ['<Super>KP_2']
+tile-bottomleft-quarter=['<Super>KP_1']
+tile-bottomright-quarter=['<Super>KP_3']
+tile-left-half = ['<Super>KP_4']
+tile-maximize = ['<Super>Up']
+tile-right-half = ['<Super>KP_6']
+tile-top-half = ['<Super>KP_8']
+tile-topleft-quarter = ['<Super>KP_7']
+tile-topright-quarter=['<Super>KP_9']
+tiling-popup-all-workspace = false
 
 [org/gnome/system/location]
 enabled = true
@@ -56,5 +114,6 @@ show-hidden = true
 sort-directories-first = true
 
 [org/gtk/settings/file-chooser]
+clock-format = '24h'
 show-hidden = true
 EOF
