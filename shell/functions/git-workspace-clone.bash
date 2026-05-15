f21a9f72ee4ee1957f3cd528785deb72892c57c8
Josh Spence <josh@spence.com.au>
2026-05-15 18:03:09 +1000

    Change `git-workspace-clone` to a function

---
+++ shell/functions/git-workspace-clone.bash
@@ -0,0 +1,12 @@
+function git-workspace-clone() {
+  local repository="${1?Repository not specified}"
+  local destination="${HOME}/workspace/$(sed --regexp-extended 's|\.git$||; s|^([a-z+]+://)?([^@]+@)?([^/:]+)[:/](.+)$|\3/\4|' <<< "${repository}")"
+
+  if test -d "${destination}"; then
+    echo "Repository ${repository} has already been cloned to ${destination}" >&2
+    return 1
+  fi
+
+  git clone "${repository}" "${destination}"
+  cd "${destination}"
+}
