d24565c0db79c0220e7a8f8e25fff208b4de8487
Josh Spence <josh@spence.com.au>
2026-05-29 22:04:53 +1000

    Lint fix

--- shell/functions/git-workspace-clone.bash
+++ shell/functions/git-workspace-clone.bash
@@ -2,7 +2,7 @@
 
 function git-workspace-clone() {
   local repository="${1?Repository not specified}"
-  local destination="${HOME}/workspace/$(sed --regexp-extended 's|\.git$||; s|^([a-z+]+://)?([^@]+@)?([^/:]+)[:/](.+)$|\3/\4|' <<< "${repository}")"
+  local destination="${HOME}/workspace/$(sed --regexp-extended 's|\.git$||; s|^([a-z+]+://)?([^@]+@)?([^/:]+)[:/](.+)$|\3/\4|' <<<"${repository}")"
 
   if test -d "${destination}"; then
     echo "Repository ${repository} has already been cloned to ${destination}" >&2
