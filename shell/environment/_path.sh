524e4469dc96c6e437617257803ca4999c4a3c93
Josh Spence <josh@spence.com.au>
2026-08-25 11:13:23 +1000

    Remove `~/.venv/bin` from `$PATH`

--- shell/environment/_path.sh
+++ shell/environment/_path.sh
@@ -2,10 +2,6 @@
 
 PATH="${HOME}/.local/bin:${PATH}"
 
-if test -d "${HOME}/.venv"; then
-  export PATH="${HOME}/.venv/bin:${PATH}"
-fi
-
 if command -v go >/dev/null; then
   PATH="$(go env GOPATH)/bin:${PATH}"
 fi
